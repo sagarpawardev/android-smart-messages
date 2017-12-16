db81c4c7a2f85a66a9a531508b757043284a7ace
sagarpawardev <sagar.pawar.dev@gmail.com>
2017-12-16 14:01:45 +0530

    XML Support for NotificationView

--- app/src/main/java/dev/sagar/smsblocker/ux/customviews/NotificationView.java
+++ app/src/main/java/dev/sagar/smsblocker/ux/customviews/NotificationView.java
@@ -1,6 +1,7 @@
 package dev.sagar.smsblocker.ux.customviews;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
@@ -24,84 +25,176 @@ public class NotificationView extends RelativeLayout {
     private LogUtil log = new LogUtil(this.getClass().getName());
 
     //Java Android
-    TextView tvTitle;
-    TextView tvDesc;
-    ImageButton closeBtn;
+    private TextView tvTitle;
+    private TextView tvDesc;
+    private ImageButton closeBtn;
+
+    //Constants
+    private static String DEFAULT_TEXT_COLOR = "#000000";
+    private static String DEFAULT_COLOR = "#FFFFFF";
 
 
     public NotificationView(Context context) {
         super(context);
-        initializeViews(context);
+        initializeViews(context, null, 0, 0);
     }
 
     public NotificationView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initializeViews(context);
+        initializeViews(context, attrs, 0,0 );
     }
 
     public NotificationView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initializeViews(context);
+        initializeViews(context, attrs, defStyleAttr, 0);
     }
 
     public NotificationView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        initializeViews(context);
+        initializeViews(context, attrs, defStyleAttr, defStyleRes);
     }
 
-    private void initializeViews(Context context) {
+    private void initializeViews(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        final String methodName = "initializeViews()";
+        log.justEntered(methodName);
+
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.compoundview_notification, this);
+        View view = inflater.inflate(R.layout.compoundview_notification, this);
+
+        tvTitle = view.findViewById(R.id.tv_title);
+        tvDesc = view.findViewById(R.id.tv_desc);
+        closeBtn = view.findViewById(R.id.imgbtn_close);
+        if(attrs!=null){
+            log.debug(methodName, "Applying Attributes");
+
+            TypedArray typedArray = context.obtainStyledAttributes(attrs,
+                    R.styleable.NotificationView, defStyleAttr, defStyleRes);
+
+            int defaultTextColor = Color.parseColor(DEFAULT_TEXT_COLOR);
+            int defaultColor = Color.parseColor(DEFAULT_COLOR);
+            String titleText = typedArray.getString(R.styleable.NotificationView_titleText);
+            int titleColor = typedArray.getColor(R.styleable.NotificationView_titleColor, defaultTextColor);
+            String descText = typedArray.getString(R.styleable.NotificationView_descText);
+            int descColor = typedArray.getColor(R.styleable.NotificationView_descColor, defaultTextColor);
+            int closeButtonColor = typedArray.getColor(R.styleable.NotificationView_closeBtnColor, defaultColor);
+            Drawable closeBtnBackground = typedArray.getDrawable(R.styleable.NotificationView_closeBtnBackground);
+
+            setTitleText(titleText);
+            setTitleColor(titleColor);
+            setDescText(descText);
+            setDescColor(descColor);
+            setCloseBtnColor(closeButtonColor);
+            setCloseBtnBackground(closeBtnBackground);
+
+            typedArray.recycle();
+        }
+
+        log.returning(methodName);
     }
 
 
     //---- Public Methods Start ----
     public void setTitleText(String txt){
+        final String methodName = "setTitleText(String)";
+        log.justEntered(methodName);
+
         tvTitle.setText(txt);
+
+        log.returning(methodName);
     }
 
     public void setTitleText(int txtId){
+        final String methodName = "setTitleText(int)";
+        log.justEntered(methodName);
+
         String txt = getContext().getString(txtId);
         setTitleText(txt);
+
+        log.returning(methodName);
     }
 
     public void setDescText(String txt){
+        final String methodName = "setDescText(String)";
+        log.justEntered(methodName);
+
         tvDesc.setText(txt);
+
+        log.returning(methodName);
     }
 
     public void setDescText(int txtId){
+        final String methodName = "setDescText(int)";
+        log.justEntered(methodName);
+
         String txt = getContext().getString(txtId);
         setDescText(txt);
+
+        log.returning(methodName);
     }
 
     public void setTitleColor(int colorId){
+        final String methodName = "setTitleColor()";
+        log.justEntered(methodName);
+
         tvTitle.setTextColor(colorId);
+
+        log.returning(methodName);
     }
 
     public void setDescColor(int colorId){
+        final String methodName = "setDescColor()";
+        log.justEntered(methodName);
+
         tvDesc.setTextColor(colorId);
+
+        log.returning(methodName);
     }
 
     public void setCloseBtnColor(int colorId){
+        final String methodName = "setCloseBtnColor()";
+        log.justEntered(methodName);
+
         closeBtn.setColorFilter(colorId);
+
+        log.returning(methodName);
     }
 
 
     public void setCloseBtnBackground(Drawable drawable){
+        final String methodName = "setCloseBtnBackground()";
+        log.justEntered(methodName);
+
         closeBtn.setBackground(drawable);
+
+        log.returning(methodName);
     }
 
     public void setCloseBtnBackgroundColor(int colorId){
+        final String methodName = "setCloseBtnBackgroundColor()";
+        log.justEntered(methodName);
+
         closeBtn.setBackgroundColor(colorId);
+
+        log.returning(methodName);
     }
 
     public void setCloseBtnBackgroundResource(int resId){
+        final String methodName = "setCloseBtnBackgroundResource()";
+        log.justEntered(methodName);
+
         closeBtn.setBackgroundResource(resId);
+
+        log.returning(methodName);
     }
 
     public void setOnCloseClickListener(OnClickListener listener){
+        final String methodName = "setOnCloseClickListener()";
+        log.justEntered(methodName);
+
         closeBtn.setOnClickListener(listener);
+
+        log.returning(methodName);
     }
 
     /*@Override
@@ -111,15 +204,4 @@ public class NotificationView extends RelativeLayout {
     }*/
 
     //---- Public Methods End ----
-
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-
-        tvTitle = findViewById(R.id.tv_title);
-        tvDesc = findViewById(R.id.tv_desc);
-        closeBtn = findViewById(R.id.imgbtn_close);
-
-
-    }
 }
