08403fdc120642ec33d2c32277b15ff0bdee8f17
sagarpawardev <sagar.pawar.dev@gmail.com>
2018-03-22 02:55:37 +0530

    Support for #55. Swipe to Unstar Bug fix on DP click

--- app/src/main/java/dev/sagar/smsblocker/ux/activities/StarredSMSActivity.java
+++ app/src/main/java/dev/sagar/smsblocker/ux/activities/StarredSMSActivity.java
@@ -1,5 +1,11 @@
 package dev.sagar.smsblocker.ux.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -26,8 +33,11 @@ public class StarredSMSActivity extends AppCompatActivity implements RVStarredSM
     private LogUtil log = new LogUtil(this.getClass().getName());
 
     //Java Android
+    private View parentHolder;
     private RecyclerView rvStarredSMS;
     private RVStarredSMSAdapter adapter;
+    private LinearLayoutManager layoutManager;
+    private Paint p = new Paint();
 
     //Java Core
     private List<SMS> smses;
@@ -38,6 +48,7 @@ public class StarredSMSActivity extends AppCompatActivity implements RVStarredSM
         final String methodName =  "init()";
         log.justEntered(methodName);
 
+        parentHolder = findViewById(R.id.view_holder);
         rvStarredSMS = (RecyclerView) findViewById(R.id.rv_starred_sms);
         inboxUtil = new InboxUtil(this);
 
@@ -65,10 +76,88 @@ public class StarredSMSActivity extends AppCompatActivity implements RVStarredSM
         log.justEntered(methodName);
         adapter = new RVStarredSMSAdapter(this, smses, this);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        RVDragCallback dragCallback = new RVDragCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        new ItemTouchHelper(dragCallback).attachToRecyclerView(rvStarredSMS);
+
+        layoutManager = new LinearLayoutManager(this);
         rvStarredSMS.setLayoutManager(layoutManager);
         rvStarredSMS.setAdapter(adapter);
         log.returning(methodName);
+    }
+
+
+
+    class RVDragCallback extends ItemTouchHelper.SimpleCallback {
+
+        public RVDragCallback(int dragDirs, int swipeDirs) {
+            super(dragDirs, swipeDirs);
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            final int position = viewHolder.getAdapterPosition();
+            final SMS sms = adapter.itemRemoved(position);
+
+            Snackbar.make(parentHolder, "SMS Unstarred", Snackbar.LENGTH_LONG)
+                    .setAction(R.string.label_undo, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            adapter.restore(position, sms);
+                            int topVisiblePosition = layoutManager.findFirstVisibleItemPosition();
+                            int lastVisiblePosition = layoutManager.findLastVisibleItemPosition();
+                            if(position < topVisiblePosition || position > lastVisiblePosition ) {
+                                rvStarredSMS.smoothScrollToPosition(position);
+                            }
+                        }
+                    })
+            .show();
+        }
+
+        @Override
+        public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            /*Bitmap icon;
+            if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
+
+                View itemView = viewHolder.itemView;
+                float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                float width = 25;
+
+                if(dX > 0){
+                    p.setColor(Color.parseColor("#388E3C"));
+                    RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
+                    c.drawRect(background,p);
+                    icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_star_white_24dp);
+                    RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) 50,(float) itemView.getLeft()+ 2*width,(float)50 );
+                    //c.drawBitmap(icon,null,icon_dest,p);
+                    drawText("Unstar", c, icon_dest, p);
+                } else {
+                    p.setColor(Color.parseColor("#D32F2F"));
+                    RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
+                    c.drawRect(background,p);
+                    icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_star_white_24dp);
+                    RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
+                    c.drawBitmap(icon,null,icon_dest,p);
+                    drawText("Unstar", c, icon_dest, p);
+                }
+            }*/
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+
+        private void drawText(String text, Canvas c, RectF button, Paint p) {
+            float textSize = 60;
+            p.setColor(Color.WHITE);
+            p.setAntiAlias(true);
+            p.setTextSize(textSize);
+
+            float textWidth = p.measureText(text);
+            c.drawText(text, button.centerX()-(textWidth/2), button.centerY()+(textSize/2), p);
+        }
     }
 
     //--- AppCompatActivity Overrides Starts ---
--- app/src/main/java/dev/sagar/smsblocker/ux/adapters/RVStarredSMSAdapter.java
+++ app/src/main/java/dev/sagar/smsblocker/ux/adapters/RVStarredSMSAdapter.java
@@ -5,14 +5,18 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Handler;
 import android.provider.ContactsContract;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import dev.sagar.smsblocker.R;
@@ -22,6 +26,7 @@ import dev.sagar.smsblocker.tech.beans.SMS;
 import dev.sagar.smsblocker.tech.exceptions.ReadContactPermissionException;
 import dev.sagar.smsblocker.tech.utils.ContactUtilSingleton;
 import dev.sagar.smsblocker.tech.utils.DateUtilSingleton;
+import dev.sagar.smsblocker.tech.utils.InboxUtil;
 import dev.sagar.smsblocker.tech.utils.LogUtil;
 import dev.sagar.smsblocker.ux.customviews.DisplayPictureView;
 
@@ -36,15 +41,22 @@ public class RVStarredSMSAdapter extends RecyclerView.Adapter<RVStarredSMSAdapte
 
     //Java Android
     private Context context;
+    private Handler deleteHandler;
 
     //Java Core
     private List<SMS> smses;
     private Callback callback;
+    private InboxUtil inboxUtil;
+
+    //Constants
+    private final long TIMEOUT_TIME = 3500; //3.5 Seconds equal to Snackbar.LENGTH_LONG
 
     public RVStarredSMSAdapter(Context context, List<SMS> smses, Callback callback) {
         this.context = context;
         this.smses = smses;
         this.callback = callback;
+
+        inboxUtil = new InboxUtil(context);
     }
 
     @Override
@@ -148,17 +160,28 @@ public class RVStarredSMSAdapter extends RecyclerView.Adapter<RVStarredSMSAdapte
             parent = view;
 
             dpView.setOnClickListener(new View.OnClickListener() {
+                //Log Initiate
+                private LogUtil log = new LogUtil(this.getClass().getName());
+
                 @Override
                 public void onClick(View view) {
+                    final String methodName =  "onClick(View)";
+                    log.justEntered(methodName);
 
-                    //FIXME On click gives error
+                    int position = getAdapterPosition();
+                    SMS sms = smses.get(position);
+                    String threadId = sms.getAddress();
+
+                    log.info(methodName, "Clicked Position: "+position+" Address: "+threadId);
+
                     String contactID = null;
                     try {
-                        String threadId = tvAddress.getText().toString();
                         Contact contact = ContactUtilSingleton.getInstance().getContact(context, threadId);
                         contactID = contact.getId();
+                        log.info(methodName, "Got Contact ID: "+contactID);
                     } catch (ReadContactPermissionException e) {
                         e.printStackTrace();
+                        log.error(methodName, "Fall into Exception: "+e.getMessage());
                     }
 
                     if(contactID!=null) {
@@ -170,6 +193,41 @@ public class RVStarredSMSAdapter extends RecyclerView.Adapter<RVStarredSMSAdapte
                 }
             });
         }
+    }
+
+    public SMS itemRemoved(int position){
+        final String methodName =  "itemRemoved(int)";
+        log.justEntered(methodName);
+
+        final SMS sms = smses.remove(position);
+        notifyItemRemoved(position);
+
+        deleteHandler = new Handler();
+        deleteHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                List<SMS> list = new ArrayList<>();
+                list.add(sms);
+                int count = inboxUtil.unstarSMSes(list);
+            }
+        }, TIMEOUT_TIME);
+
+        log.returning(methodName);
+        return sms;
+    }
+
+    public void restore(int position, SMS sms){
+        final String methodName =  "restore(int , SMS)";
+        log.justEntered(methodName);
+
+        //Cancel Deletion
+        deleteHandler.removeCallbacksAndMessages(null);
+
+        //Reflect in UI
+        smses.add(position, sms);
+        notifyItemInserted(position);
+
+        log.returning(methodName);
     }
 
     public interface Callback{
